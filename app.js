8aaa427e0d09c92de36ef53d40819015ecb4f894
Lauramars <lauramars85@gmail.com>
2020-09-07 15:53:00 +0200

    Fetch with Promises

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = new easyHTTP();
+// const http = new easyHTTP();
 
 // GET posts
 // http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
@@ -19,10 +19,10 @@ const http = new easyHTTP();
 // });
 
 // Create Data
-const data = {
-  title: 'Custom Post',
-  body: 'This is a custom post',
-};
+// const data = {
+//   title: 'Custom Post',
+//   body: 'This is a custom post',
+// };
 
 // Create Post
 // http.post('https://jsonplaceholder.typicode.com/posts', data, function (
@@ -49,13 +49,46 @@ const data = {
 // });
 
 // Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function (
-  err,
-  response
-) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function (
+//   err,
+//   response
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+const http = new EasyHTTP();
+
+// GET USERS
+// http
+//   .get('https://jsonplaceholder.typicode.com/users')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// USER DATA
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com',
+};
+
+// CREATE USER
+// http
+//   .post('https://jsonplaceholder.typicode.com/users', data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// UPDATE USER
+// http
+//   .put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+
+// DELETE USER
+http
+  .delete('https://jsonplaceholder.typicode.com/users/2')
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
